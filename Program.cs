a0d667c9f3cc0e78d238cddb48557f088bcf3348
Татьяна Красненкова <tatanakrasnenkova@MacBook-Air-Tatana.local>
2022-09-21 12:05:15 +0300

    add new metod1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
